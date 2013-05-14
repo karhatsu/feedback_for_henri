df02bde400720d223d04966d74f106b22a9f5da1
Henri Karhatsu <henri@karhatsu.com>
2013-05-14 07:59:50 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeedbackForHenri::Application.config.secret_key_base = 'fc01a9e683b4735cec70684243484a08429d8017903307bd091f7310e1d3ebc8e14c81c7eae19ab8c5376508b4f509b5c36afbd4515231f443c300452bd570ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FeedbackForHenri::Application.config.session_store :cookie_store, key: '_feedback_for_henri_session'
