98e1d47ea64d07213d3298796b486296c9f32556
Henri Karhatsu <henri@karhatsu.com>
2013-05-15 16:43:12 +0300

    project url so that you cannot guess it easily

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def show
-    @project = Project.where(key: params[:id]).first
+    @project = Project.find(params[:id])
     redirect_to new_project_answer_path(@project)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   has_many :answers
   
   validates :key, :presence => true
+  
+  def to_param
+    "#{id}-#{key}"
+  end
 end
