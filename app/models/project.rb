b9267563654a76c5a0dc2ad9f177c7febf733891
Henri Karhatsu <henri@karhatsu.com>
2013-05-15 17:22:38 +0300

    net promoter score to admin page

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,21 @@ class Project < ActiveRecord::Base
   def to_param
     "#{id}-#{key}"
   end
+  
+  def net_promoter_score
+    return nil if answers.empty?
+    negatives = 0
+    neutrals = 0
+    positives = 0
+    answers.each do |answer|
+      if answer.score >= 9
+        positives = positives + 1
+      elsif answer.score == 7 or answer.score == 8
+        neutrals = neutrals + 1
+      else
+        negatives = negatives + 1
+      end
+    end
+    100 * (-negatives + positives).to_f / (negatives + neutrals + positives)
+  end
 end
